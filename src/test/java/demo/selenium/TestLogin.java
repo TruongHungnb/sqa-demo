0c3b28113c54c3c5f48ba08eb880e5976156bb2f
tongthidan <tongthidan99@gmail.com>
2021-05-26 21:07:03 +0700

    update selenium

--- src/test/java/demo/selenium/TestLogin.java
+++ src/test/java/demo/selenium/TestLogin.java
@@ -34,121 +34,336 @@ public class TestLogin {
 	String url = "http://localhost:9000/login";
 	String USERNAME = "admin";
 	String PASSWORD = "admin";
+	String USERNAME2 = "admin";
+	String PASSWORD2 = "1";
+	String USERNAME3 = "1";
+	String PASSWORD3 = "admin";
+	String USERNAME4 = "admin";
+	String PASSWORD4 = "";
+	String USERNAME5 = "";
+	String PASSWORD5 = "admin";
+	String USERNAME6 = "";
+	String PASSWORD6 = "";
+	String USERNAME7 = " admin";
+	String PASSWORD7 = "admin";
+	String USERNAME8 = "admin";
+	String PASSWORD8 = " admin";
+	String USERNAME9 = "  ";
+	String PASSWORD9 = "  ";
 	
-	String USERNAME2 = "user";
-	String PASSWORD2 = "user";
 	Map<String, Object[]> testNGResults;
 	HSSFWorkbook workbook;
 	HSSFSheet sheet;
 
-
-
-
-	@Test(priority=1)
-	public void testfirst() {
+	@Test(priority = 1)
+	public void LoginDungThongTin() {
 
 		try {
-			
-			
-			
 			driver = new ChromeDriver();
 			driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 			driver.get(url);
 			driver.manage().window().maximize();
-			
 			driver.navigate().refresh();
-			
-			
 			WebElement taikhoan = driver.findElement(By.name("username"));
 			taikhoan.sendKeys(USERNAME);
 			WebElement matkhau = driver.findElement(By.name("password"));
 			matkhau.sendKeys(PASSWORD);
 			WebElement login = driver.findElement(By.className("btn-primary"));
 			login.click();
-			
+			WebElement noti = driver.findElement(By.id("home-logged-message"));
+			Assert.assertEquals(noti.getText(), "Bạn đang đăng nhập với tài khoản \"admin\".");
+			if ((noti.getText()).equals("Bạn đang đăng nhập với tài khoản \"admin\".")) {
+				testNGResults.put("2", new Object[] { "1", "Nhập đúng thông tin đăng nhập",
+						"Bạn đang đăng nhập với tài khoản admin", "Pass" });
+			}
 			Thread.sleep(1000);
-			testNGResults.put("2", new Object[] { "1", "nhap dung thong tin ", "login thanh cong-1", "Pass" });
-		
+
 		} catch (Exception e) {
 			// TODO: handle exception
-			testNGResults.put("2", new Object[] {"1", "nhap sai thong tin", "login that bai-1", "Fail"});
-			Assert.assertTrue(false);
 		}
 		driver.close();
-		
-		
 	}
-	
-	
-	
-	@Test(priority=2)
-	public void test2() {
 
+	@Test(priority = 2)
+	public void LoginSaiPassword() {
 		try {
-			
-			WebDriverManager.chromedriver().setup();
-			
-			
-			
+//			WebDriverManager.chromedriver().setup();
 			driver = new ChromeDriver();
 			driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 			driver.get(url);
 			driver.manage().window().maximize();
-			
 			driver.navigate().refresh();
-			
 			WebElement taikhoan = driver.findElement(By.name("username"));
 			taikhoan.sendKeys(USERNAME2);
 			WebElement matkhau = driver.findElement(By.name("password"));
 			matkhau.sendKeys(PASSWORD2);
 			WebElement login = driver.findElement(By.className("btn-primary"));
 			login.click();
-			
+			WebElement noti = driver.findElement(By.className("alert-danger"));
+			Assert.assertEquals(noti.getText(),
+					"Đăng nhập thất bại Vui lòng kiểm tra thông tin đăng nhập của bạn và đăng nhập lại.");
+			if ((noti.getText())
+					.equals("Đăng nhập thất bại Vui lòng kiểm tra thông tin đăng nhập của bạn và đăng nhập lại.")) {
+				testNGResults.put("3", new Object[] { "2", "Nhập sai Password",
+						"Đăng nhập thất bại Vui lòng kiểm tra thông tin đăng nhập của bạn và đăng nhập lại.", "Pass" });
+			}
 			Thread.sleep(1000);
-			testNGResults.put("3", new Object[] { "2", "nhap dung thong tin", "login thanh cong-2", "Pass" });
-		
 		} catch (Exception e) {
 			// TODO: handle exception
-			testNGResults.put("3", new Object[] {"2", "nhap sai thong tin", "login that bai-2", "Fail"});
-			Assert.assertTrue(false);
 		}
-		
 		driver.close();
 	}
 
-	@Test(priority=3)
-	public void test3() {
+	@Test(priority = 3)
+	public void LoginSaiUsername() {
 
 		try {
-			
-			WebDriverManager.chromedriver().setup();
-			
-			
 			driver = new ChromeDriver();
 			driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 			driver.get(url);
 			driver.manage().window().maximize();
-			
 			driver.navigate().refresh();
-			
 			WebElement taikhoan = driver.findElement(By.name("username"));
-			taikhoan.sendKeys(USERNAME2);
+			taikhoan.sendKeys(USERNAME3);
 			WebElement matkhau = driver.findElement(By.name("password"));
-			matkhau.sendKeys(PASSWORD2);
+			matkhau.sendKeys(PASSWORD3);
 			WebElement login = driver.findElement(By.className("btn-primary"));
 			login.click();
-			
+			WebElement noti = driver.findElement(By.className("alert-danger"));
+			Assert.assertEquals(noti.getText(),
+					"Đăng nhập thất bại Vui lòng kiểm tra thông tin đăng nhập của bạn và đăng nhập lại.");
+			if ((noti.getText())
+					.equals("Đăng nhập thất bại Vui lòng kiểm tra thông tin đăng nhập của bạn và đăng nhập lại.")) {
+				testNGResults.put("4", new Object[] { "3", "Nhập sai Username",
+						"Đăng nhập thất bại Vui lòng kiểm tra thông tin đăng nhập của bạn và đăng nhập lại.", "Pass" });
+			}
 			Thread.sleep(1000);
-			testNGResults.put("4", new Object[] { "3", "nhap dung thong tin", "login thanh cong-3", "Pass" });
-		
+
 		} catch (Exception e) {
 			// TODO: handle exception
-			testNGResults.put("4", new Object[] {"3", "nhap sai thong tin", "login that bai-3", "Fail"});
-			Assert.assertTrue(false);
 		}
-		
 		driver.close();
 	}
+	
+	@Test(priority = 4)
+	public void KhongNhapUsername() {
+
+		try {
+			driver = new ChromeDriver();
+			driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+			driver.get(url);
+			driver.manage().window().maximize();
+			driver.navigate().refresh();
+			WebElement taikhoan = driver.findElement(By.name("username"));
+			taikhoan.sendKeys(USERNAME4);
+			WebElement matkhau = driver.findElement(By.name("password"));
+			matkhau.sendKeys(PASSWORD4);
+			WebElement login = driver.findElement(By.className("btn-primary"));
+			login.click();
+			WebElement noti = driver.findElement(By.className("alert-danger"));
+			Assert.assertEquals(noti.getText(),
+					"Đăng nhập thất bại Vui lòng kiểm tra thông tin đăng nhập của bạn và đăng nhập lại.");
+			if ((noti.getText())
+					.equals("Đăng nhập thất bại Vui lòng kiểm tra thông tin đăng nhập của bạn và đăng nhập lại.")) {
+				testNGResults.put("5", new Object[] { "4", "Không nhập Username",
+						"Đăng nhập thất bại Vui lòng kiểm tra thông tin đăng nhập của bạn và đăng nhập lại.", "Pass" });
+			}
+			Thread.sleep(1000);
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		driver.close();
+	}
+	
+	@Test(priority = 5)
+	public void KhongNhapPassword() {
+
+		try {
+			driver = new ChromeDriver();
+			driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+			driver.get(url);
+			driver.manage().window().maximize();
+			driver.navigate().refresh();
+			WebElement taikhoan = driver.findElement(By.name("username"));
+			taikhoan.sendKeys(USERNAME5);
+			WebElement matkhau = driver.findElement(By.name("password"));
+			matkhau.sendKeys(PASSWORD5);
+			WebElement login = driver.findElement(By.className("btn-primary"));
+			login.click();
+			WebElement noti = driver.findElement(By.className("alert-danger"));
+			Assert.assertEquals(noti.getText(),
+					"Đăng nhập thất bại Vui lòng kiểm tra thông tin đăng nhập của bạn và đăng nhập lại.");
+			if ((noti.getText())
+					.equals("Đăng nhập thất bại Vui lòng kiểm tra thông tin đăng nhập của bạn và đăng nhập lại.")) {
+				testNGResults.put("6", new Object[] { "5", "Không nhập Password",
+						"Đăng nhập thất bại Vui lòng kiểm tra thông tin đăng nhập của bạn và đăng nhập lại.", "Pass" });
+			}
+			Thread.sleep(1000);
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		driver.close();
+	}
+	
+	@Test(priority = 6)
+	public void KhongNhapUsernamePassword() {
+
+		try {
+			driver = new ChromeDriver();
+			driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+			driver.get(url);
+			driver.manage().window().maximize();
+			driver.navigate().refresh();
+			WebElement taikhoan = driver.findElement(By.name("username"));
+			taikhoan.sendKeys(USERNAME6);
+			WebElement matkhau = driver.findElement(By.name("password"));
+			matkhau.sendKeys(PASSWORD6);
+			WebElement login = driver.findElement(By.className("btn-primary"));
+			login.click();
+			WebElement noti = driver.findElement(By.className("alert-danger"));
+			Assert.assertEquals(noti.getText(),
+					"Đăng nhập thất bại Vui lòng kiểm tra thông tin đăng nhập của bạn và đăng nhập lại.");
+			if ((noti.getText())
+					.equals("Đăng nhập thất bại Vui lòng kiểm tra thông tin đăng nhập của bạn và đăng nhập lại.")) {
+				testNGResults.put("7", new Object[] { "6", "Không nhập cả Username và Password",
+						"Đăng nhập thất bại Vui lòng kiểm tra thông tin đăng nhập của bạn và đăng nhập lại.", "Pass" });
+			}
+			Thread.sleep(1000);
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		driver.close();
+	}
+	
+	@Test(priority = 7)
+	public void KhoangTrangTruocUsername() {
+
+		try {
+			driver = new ChromeDriver();
+			driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+			driver.get(url);
+			driver.manage().window().maximize();
+			driver.navigate().refresh();
+			WebElement taikhoan = driver.findElement(By.name("username"));
+			taikhoan.sendKeys(USERNAME7);
+			WebElement matkhau = driver.findElement(By.name("password"));
+			matkhau.sendKeys(PASSWORD7);
+			WebElement login = driver.findElement(By.className("btn-primary"));
+			login.click();
+			WebElement noti = driver.findElement(By.className("alert-danger"));
+			Assert.assertEquals(noti.getText(),
+					"Đăng nhập thất bại Vui lòng kiểm tra thông tin đăng nhập của bạn và đăng nhập lại.");
+			if ((noti.getText())
+					.equals("Đăng nhập thất bại Vui lòng kiểm tra thông tin đăng nhập của bạn và đăng nhập lại.")) {
+				testNGResults.put("8", new Object[] { "7", "Trước Username có khoảng trắng",
+						"Đăng nhập thất bại Vui lòng kiểm tra thông tin đăng nhập của bạn và đăng nhập lại.", "Pass" });
+			}
+			Thread.sleep(1000);
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		driver.close();
+	}
+	
+	@Test(priority = 8)
+	public void KhoangTrangTruocPassword() {
+
+		try {
+			driver = new ChromeDriver();
+			driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+			driver.get(url);
+			driver.manage().window().maximize();
+			driver.navigate().refresh();
+			WebElement taikhoan = driver.findElement(By.name("username"));
+			taikhoan.sendKeys(USERNAME8);
+			WebElement matkhau = driver.findElement(By.name("password"));
+			matkhau.sendKeys(PASSWORD8);
+			WebElement login = driver.findElement(By.className("btn-primary"));
+			login.click();
+			WebElement noti = driver.findElement(By.className("alert-danger"));
+			Assert.assertEquals(noti.getText(),
+					"Đăng nhập thất bại Vui lòng kiểm tra thông tin đăng nhập của bạn và đăng nhập lại.");
+			if ((noti.getText())
+					.equals("Đăng nhập thất bại Vui lòng kiểm tra thông tin đăng nhập của bạn và đăng nhập lại.")) {
+				testNGResults.put("9", new Object[] { "8", " Trước Password có khoảng trắng",
+						"Đăng nhập thất bại Vui lòng kiểm tra thông tin đăng nhập của bạn và đăng nhập lại.", "Pass" });
+			}
+			Thread.sleep(1000);
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		driver.close();
+	}
+	
+	@Test(priority = 9)
+	public void UsernamePasswordLaKhoangTrang() {
+
+		try {
+			driver = new ChromeDriver();
+			driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+			driver.get(url);
+			driver.manage().window().maximize();
+			driver.navigate().refresh();
+			WebElement taikhoan = driver.findElement(By.name("username"));
+			taikhoan.sendKeys(USERNAME9);
+			WebElement matkhau = driver.findElement(By.name("password"));
+			matkhau.sendKeys(PASSWORD9);
+			WebElement login = driver.findElement(By.className("btn-primary"));
+			login.click();
+			WebElement noti = driver.findElement(By.className("alert-danger"));
+			Assert.assertEquals(noti.getText(),
+					"Đăng nhập thất bại Vui lòng kiểm tra thông tin đăng nhập của bạn và đăng nhập lại.");
+			if ((noti.getText())
+					.equals("Đăng nhập thất bại Vui lòng kiểm tra thông tin đăng nhập của bạn và đăng nhập lại.")) {
+				testNGResults.put("10", new Object[] { "9", "Username và Password là khoảng trắng",
+						"Đăng nhập thất bại Vui lòng kiểm tra thông tin đăng nhập của bạn và đăng nhập lại.", "Pass" });
+			}
+			Thread.sleep(1000);
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		driver.close();
+	}
+	
+
+	@Test(priority = 10)
+	public void LoginWithRemember() {
+
+		try {
+
+			driver = new ChromeDriver();
+			driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+			driver.get(url);
+			driver.manage().window().maximize();
+			driver.navigate().refresh();
+			WebElement taikhoan = driver.findElement(By.name("username"));
+			taikhoan.sendKeys(USERNAME);
+			WebElement matkhau = driver.findElement(By.name("password"));
+			matkhau.sendKeys(PASSWORD);
+			WebElement remember = driver.findElement(By.name("rememberMe"));
+			remember.click();
+			WebElement login = driver.findElement(By.className("btn-primary"));
+			login.click();
+			WebElement noti = driver.findElement(By.id("home-logged-message"));
+			Assert.assertEquals(noti.getText(), "Bạn đang đăng nhập với tài khoản \"admin\".");
+			if ((noti.getText()).equals("Bạn đang đăng nhập với tài khoản \"admin\".")) {
+				testNGResults.put("11", new Object[] { "10", "Nhập đúng thông tin đăng nhập và Click nhớ mật khẩu",
+						"Bạn đang đăng nhập với tài khoản admin", "Pass" });
+			}
+			Thread.sleep(1000);
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		driver.close();
+	}
+
 	
 	
 	@BeforeClass
@@ -157,7 +372,7 @@ public class TestLogin {
 		workbook = new HSSFWorkbook();
 		sheet = workbook.createSheet("TestNg Result");
 		testNGResults = new LinkedHashMap<String, Object[]>();
-		testNGResults.put("1", new Object[] { "test step no.", "action", "expect output", "actual output" });
+		testNGResults.put("1", new Object[] { "STT", "Tên Testcase", "Kết quả mong muốn", "Kết quả" });
 
 		try {
 			driver = new ChromeDriver();
@@ -169,7 +384,6 @@ public class TestLogin {
 		}
 		driver.close();
 	}
-
 
 	@AfterTest
 	public void Writeclose() {
@@ -203,7 +417,8 @@ public class TestLogin {
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
-	
+//	driver.close();
+//	driver.quit();
 	}
 
 }
